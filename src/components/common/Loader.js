749199286fd5d93c437742052792eb2a8fbc8d12
Abhishek <abhivision3@gmail.com>
2026-04-03 18:45:23 +0530

    Update Loader.js

--- src/components/common/Loader.js
+++ src/components/common/Loader.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
-
+// loader 
 export default function Loader() {
     return (
         <Box sx={{ width: '100%' }}>
